9a3ce32e02e0068a0c11432f7469290ff8933883
David Krauth <dakrauth@gmail.com>
2021-02-27 13:32:08 -1000

    corrected re path

--- src/travel/api/urls.py
+++ src/travel/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path('^logs/([-\w]+)/$', views.LogListView.as_view(), name='user_log_api'),
-    re_path('^flag-game/$', views.FlagGameView.as_view(), name='flag_game_api'),
+    re_path(r'^logs/([-\w]+)/$', views.LogListView.as_view(), name='user_log_api'),
+    re_path(r'^flag-game/$', views.FlagGameView.as_view(), name='flag_game_api'),
 ]
